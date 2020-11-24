092ce988fa005ac64f55b047406d776ada9e31fd
nii <nodenkey@gmail.com>
2020-11-24 15:22:01 +0000

    Add cors

--- app.js
+++ app.js
@@ -14,12 +14,6 @@ const app = express();
 
 app.use(cors());
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 //body parser middleware
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
