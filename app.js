322280dc176f42eadd0311066f0dc7e2de09dc35
nii <nodenkey@gmail.com>
2020-11-24 14:48:56 +0000

    Add cors

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://4th-ir-register.netlify.app/counter-register");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
